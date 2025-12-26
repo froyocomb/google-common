c8567f9aa61ef2492f6bca9cbeab3fda3304e9e4
Mainnn <101455553+main3782@users.noreply.github.com>
2025-12-26 22:08:07 +0100

    Stub Android.mk

--- Android.mk
+++ Android.mk
@@ -4,10 +4,9 @@
 # lollipop apps want it
 # so here you go...
 # all of the things below don't do anything
-# several apps just include it in their makefiles
-# why? idk, probably because google hates AOSP,
-# and in turn, their devs get access to the stupidest repo named
-# google-common
+# there's nothing else to say
+# i just hope that lollipop apps stop screamin
+# and i'm going 2 bed.
 
 LOCAL_PATH := $(call my-dir)
 
